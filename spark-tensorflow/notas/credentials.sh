cb42305450ea30d607d9df40dad094972abe7eca
Isaac <imendozak24@gmail.com>
2023-03-04 15:31:49 -0500

    Create credentials.sh

---
+++ spark-tensorflow/notas/credentials.sh
@@ -0,0 +1,10 @@
+###################################################
+### User VPN client
+###################################################
+#User: germancayo
+#Pass: gc4y0#$
+###################################################
+### RHEL 8 - VM remote
+###################################################
+#Apache Spark/Tensorflow 01	.vuce.gob.pe	192.168.130.145	8	80 GiB	24 GiB	admin	Pk$$w1rt	RHEL 8.5
+#Apache Spark/Tensorflow 02	.vuce.gob.pe	192.168.130.146	8	80 GiB	16 GiB	admin	Pk$$w2rt	RHEL 8.5
